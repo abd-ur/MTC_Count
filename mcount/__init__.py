20ed815532fc59f97363ccd75db1d5c87e024202
Abdur Rahman Ansari <115999605+abd-ur@users.noreply.github.com>
2025-03-01 11:20:51 +0530

    __init__.py

--- mcount/__init__.py
+++ mcount/__init__.py
@@ -8,5 +8,5 @@ __author__ = "Abdur Rahman Ansari"
 __license__ = "MIT"
 
 # Import key functions for easy access
-from .main import count
+from .main import circle
 # from .utils import helper_function
